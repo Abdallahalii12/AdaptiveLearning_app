c617cd4762b4db81b3512fae0aa81a197c3db14b
EL-Sakr <120565670+EL-Sakr@users.noreply.github.com>
2025-03-10 02:01:33 +0200

    Update urls.py

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import UserProfileViewSet
 
 router = DefaultRouter()
-router.register(r'profiles', UserProfileViewSet, basename='userprofile')  # ✅ تحديد `basename`
+router.register(r'profiles', UserProfileViewSet, basename='userprofile')  
 
 urlpatterns = [
     path('', include(router.urls)),
